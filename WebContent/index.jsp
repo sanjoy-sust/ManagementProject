1959342409722fb7ba4ff91017869f86ac844012
Sanjoy <Sanjoy@Sanjoy-PC.(none)>
2013-05-03 14:52:08 +0600

    initial

---
+++ WebContent/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Multi Action Controller Example Demo</title>
+</head>
+<body>
+<a href="employee/add.htm" onclick="" id="clickMe">Add Employee</a>&nbsp;|&nbsp;
+<a href="employee/update.htm" onclick="" id="clickMe">Update Employee</a>&nbsp;|&nbsp;
+<a href="employee/delete.htm" onclick="" id="clickMe">Delete Employee</a>
+
+</body>
+</html>
---
+++ src/com/springapp/UserController.java
@@ -0,0 +1,31 @@
+package com.springapp;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+public class UserController extends MultiActionController {
+
+	public ModelAndView add(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		System.out.println("Calling Add Employee...");
+		return new ModelAndView("employee", "message", "Employee Added");
+
+	}
+
+	public ModelAndView delete(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		System.out.println("Calling Delete Employee...");
+		return new ModelAndView("employee", "message", "Employee Deleted");
+
+	}
+
+	public ModelAndView update(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		System.out.println("Calling Update Employee...");
+		return new ModelAndView("employee", "message", "Employee Updated");
+
+	}
+}
